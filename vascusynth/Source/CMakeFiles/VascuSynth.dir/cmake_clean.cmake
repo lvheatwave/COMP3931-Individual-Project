2e1d157a43f16a80642d4dd55bdee764d22a0d68
ll16r4d <ll16r4d@leeds.ac.uk>
2020-02-29 20:58:26 +0000

    Oxygen Map Manipulation
    -Added OxygenMap manipulation so the roots go to specific points on the surface of the heart\n- Added the Vascusynth code in to the directory\n- Added generate_roots.py which serves as the main file for the project

---
+++ vascusynth/Source/CMakeFiles/VascuSynth.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VascuSynth.dir/NodeTable.cpp.o"
+  "CMakeFiles/VascuSynth.dir/OxygenationMap.cpp.o"
+  "CMakeFiles/VascuSynth.dir/SupplyMap.cpp.o"
+  "CMakeFiles/VascuSynth.dir/TreeDrawer.cpp.o"
+  "CMakeFiles/VascuSynth.dir/VascuSynth.cpp.o"
+  "CMakeFiles/VascuSynth.dir/VascularTree.cpp.o"
+  "VascuSynth"
+  "VascuSynth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VascuSynth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
