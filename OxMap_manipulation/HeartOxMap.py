bcfda4473e1a2ef5cb61dfa55c8e4ad12c61f132
Raitis Dzalbs <ll16r4d@leeds.ac.uk>
2020-04-29 13:52:26 +0100

    Videos
    Added 2 videos for demostration

--- OxMap_manipulation/HeartOxMap.py
+++ OxMap_manipulation/HeartOxMap.py
@@ -1,11 +1,9 @@
 import os, sys
 
-# sys.path.append('../')
-
-def oxygen_map(heart_name):
-    with open('VascuSynthLocation.txt') as location:
-        vascusynthLocation = location.read().splitlines()[0]
-    try:
+def oxygen_map(heart_name): # creates an oxygenation map based off of the coordinates in the structure of the heart file
+    with open('VascuSynthLocation.txt') as location: # open location of vascusynth
+        vascusynthLocation = location.read().splitlines()[0] #
+    try: # create the file
         os.remove(str(vascusynthLocation + "/HeartOx.txt"))
         print("Overwritting HeartOx.txt")
     except:
@@ -13,8 +11,8 @@ def oxygen_map(heart_name):
 
     file_name = heart_name.split('/')[1].split(".")[0]
 
-    with open(vascusynthLocation + '/imageNames.txt', "w") as file:
-        file.writelines(file_name)
+    with open(vascusynthLocation + '/imageNames.txt', "w") as file: # write the file name into this file so when VascuSynth
+        file.writelines(file_name)                                  # generates the data it makes the directory match the name
 
     oxmap = open(str(vascusynthLocation + "/HeartOx.txt"), "w+")
     with open (heart_name) as h:
@@ -26,11 +24,10 @@ def oxygen_map(heart_name):
     heartline = 5
     oxline = 0
 
-    firstCoordinates = []
+    firstCoordinates = [] # get the first set of coordinates and use the as root node
     firstCoordinates.append(float(heart[heartline].split()[0]) + 100)
     firstCoordinates.append(float(heart[heartline].split()[1]) + 100)
     firstCoordinates.append(float(heart[heartline].split()[2]) + 100)
-
 
     perfPoint = str("PERF_POINT: " + str(round(firstCoordinates[0], 1)) + " " + str(round(firstCoordinates[1], 1)) + " " + str(round(firstCoordinates[2], 1)) + "\n")
 
@@ -47,11 +44,11 @@ def oxygen_map(heart_name):
         if heart[heartline] == "\n" or heart[heartline][0] == "P":
             break
         else:
-            line = heart[heartline].split()
-            coordinates.append([])
-            coordinates[oxline].append(float(line[0]))
-            coordinates[oxline].append(float(line[1]))
-            coordinates[oxline].append(float(line[2]))
+            line = heart[heartline].split()                # Every Heart model is filled with (x, y, z) coordinates, and there is
+            coordinates.append([])                         # up to 3 sets on each line, therefore if the line isnt empty or is not
+            coordinates[oxline].append(float(line[0]))     # not the polygon header line, then read the first set, then try read
+            coordinates[oxline].append(float(line[1]))     # for the second then try read for the third, while managing an appropriate
+            coordinates[oxline].append(float(line[2]))     # counter for each line of data
             oxline += 1
             try:
                 coordinates.append([])
@@ -71,16 +68,16 @@ def oxygen_map(heart_name):
                 coordinates.pop()
             heartline += 1
 
-    oxmap.write("200 200 200\n0 0 0 200 200 200\n0\n")
-
-    volumefactor = 2
-
+    oxmap.write("200 200 200\n0 0 0 200 200 200\n0\n") # VascuSynth needs to know how big the space is that it will be populating
+                                                       # Previous values are overwritten by the next, so therefore we set the
+    volumefactor = 2                                   # oxygenation of the whole space to 0 to not have stray bifurcations that go
+                                                       # outside of the heart
     for i in range(0, len(coordinates)):
         corner1 = []
         corner2 = []
-        corner1.append(coordinates[i][0] - volumefactor + 100)
-        corner1.append(coordinates[i][1] - volumefactor + 100)
-        corner1.append(coordinates[i][2] - volumefactor + 100)
+        corner1.append(coordinates[i][0] - volumefactor + 100) # This section makes a box around every coordinate that we just got
+        corner1.append(coordinates[i][1] - volumefactor + 100) # so that it can basically say that all the areas around the heart
+        corner1.append(coordinates[i][2] - volumefactor + 100) # need to have maximum oxygenation
 
         corner2.append(coordinates[i][0] + volumefactor + 100)
         corner2.append(coordinates[i][1] + volumefactor + 100)
@@ -90,8 +87,8 @@ def oxygen_map(heart_name):
         oxmap.write(str(round(corner2[0], 1)) + " " + str(round(corner2[1], 1)) + " " + str(round(corner2[2], 1)) + "\n")
         oxmap.write(str(1) + "\n")
 
-    oxmap.close()
+    oxmap.close() # closes the oxygenation file to have it ready to be used by vascusynth
     h.close()
 
 if __name__ == '__main__':
-    oxygen_map("../Heart Models/pa0002_se01.vtk")
+    oxygen_map("../Heart Models/pa0002_se01.vtk") # main function to be able to test this file individually
--- generate_roots.py
+++ generate_roots.py
@@ -3,7 +3,7 @@ from OxMap_manipulation.HeartOxMap import oxygen_map as oxygen_map
 from gxl_to_vtk.GXLToVTK import gxl_to_vtk as gxl_to_vtk
 from readgrid import render_project as render_project
 
-def get_program_parameters():
+def get_program_parameters(): # get the required arguments for the software to start running
     description = 'Read an unstructured grid file.'
     epilogue = ''''''
     parser = argparse.ArgumentParser(description=description, epilog=epilogue, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -13,30 +13,29 @@ def get_program_parameters():
 
 
 def main():
-    starttime = time.time()
+    starttime = time.time() # timer to see how long the software takes to compile as it takes a while
 
-    heart_name = get_program_parameters()
+    heart_name = get_program_parameters() # get the arguments
 
-    file_name = heart_name.split('/')[1].split(".")[0]
+    file_name = heart_name.split('/')[1].split(".")[0] # get and format the name of the file to use to create an apporpriate directory for results
     if not os.path.exists('results/' + file_name):
         os.makedirs('results/' + file_name)
 
-    oxygen_map(heart_name)
+    oxygen_map(heart_name) # make oxygenation map
 
-    with open('VascuSynthLocation.txt', 'r') as location:
+    with open('VascuSynthLocation.txt', 'r') as location: # get location of VascuSynth
         vascusynthlocation = location.read().splitlines()[0]
 
-    print("Running VascuSynth")
+    print("Running VascuSynth") # run VascuSynth
     os.system("cd " + vascusynthlocation + " ; ./VascuSynth paramFiles.txt imageNames.txt 0.005")
     print("Root data generated")
 
-    gxl_to_vtk(str(vascusynthlocation + '/' + file_name + '/tree_structure.xml'), str("results/" + file_name))
+    gxl_to_vtk(str(vascusynthlocation + '/' + file_name + '/tree_structure.xml'), str("results/" + file_name)) # convert gxl coordinate file to a vtk one
     print("Root data converted from GXL format to VTK")
 
-    roots_file = "results/" + file_name + "/coordinates.vtk"
-    print("%f Seconds since start of generation" % (time.time() - starttime))
+    print("%f Seconds since start of generation" % (time.time() - starttime)) # display time it took to generate
     print("Rendering")
-    render_project(heart_name, roots_file)
+    render_project(heart_name, "results/" + file_name + "/coordinates.vtk") # displays the generated roots and the heart model as one
 
 if __name__ == '__main__':
     main()
